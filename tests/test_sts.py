6c9e1b12486c48fa494b300022d2a1f7b1cffa04
pieter_blomme_home <pieter.blomme@gmail.com>
2023-05-23 12:58:31 +0200

    threadpool - certainly not safe across processes

--- tests/test_sts.py
+++ tests/test_sts.py
@@ -5,7 +5,7 @@ import time
 import random
 import pytest
 import logging
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 from uuid import uuid4
 
 logger = logging.getLogger(__name__)
@@ -67,14 +67,12 @@ def test_sts__bad_endpoint():
 
 
 def task_set_random_config_value(uid):
-    logger.info(uid)
     sts_service = STSService()
     token = sts_service.get_serialized_token(TEST_DATA_FOLDER.joinpath("valid.acc-p12"), KEYSTORE_PASSPHRASE, KEYSTORE_SSIN)
     with sts_service.session(token, TEST_DATA_FOLDER.joinpath("valid.acc-p12"), KEYSTORE_PASSPHRASE) as session:
         sts_service.config_validator.setProperty("uid_test", uid)
-        time.sleep(random.random())
+        time.sleep(random.random()*10)
         result = sts_service.config_validator.getProperty("uid_test")
-        logger.info(result)
         assert result == uid
 
             
@@ -83,6 +81,6 @@ def test_sts__multiple_sessions():
     Unclear if the current approach is safe across multiple concurrent STS sessions.
     Probably best not to count on it, but this test seems to indicate that it's ok
     """
-    with ProcessPoolExecutor(max_workers=50) as ex:
-        ex.map(task_set_random_config_value, [str(uuid4()) for _ in range(50)])
+    with ThreadPoolExecutor(max_workers=10) as ex:
+        ex.map(task_set_random_config_value, [str(uuid4()) for _ in range(10)])
         
