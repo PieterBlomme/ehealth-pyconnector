89c7ab176f8a688e29f584db87500fb4d22a17e1
Pieter Blomme <pieter.blomme@gmail.com>
2024-01-24 21:38:38 +0100

    fixie

--- ehealth/eagreement/eagreement.py
+++ ehealth/eagreement/eagreement.py
@@ -194,7 +194,7 @@ class EAgreementService(AbstractEAgreementService):
         )
         request = self.redundant_template_render(
             template=template,
-            patient=input_model.patient,
+            patient=input_model,
             id_=id_,
             builder_func=self.GATEWAY.jvm.be.ehealth.businessconnector.mycarenet.agreement.builders.RequestObjectBuilderFactory.getEncryptedRequestObjectBuilder().buildConsultAgreementRequest
             )
