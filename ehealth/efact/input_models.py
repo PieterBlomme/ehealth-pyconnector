7a2f83dbee779d848586ca8adfc76775fe375620
Pieter Blomme <pieter.blomme@gmail.com>
2024-02-22 17:18:53 +0100

    big oops

--- ehealth/efact/input_models.py
+++ ehealth/efact/input_models.py
@@ -1416,7 +1416,7 @@ class Record51(BaseModel):
         return to_str
 
 class Record52(BaseModel):
-    record: Optional[str] = "51"
+    record: Optional[str] = "52"
     num_record: Optional[str] = "000004"
     reden_manuele_invoering: Optional[str] = "0"
     nomenclatuur: str
