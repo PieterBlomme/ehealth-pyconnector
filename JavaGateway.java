769337edbd308d8145cf8c4607fd5794074831db
Pieter Blomme <pieter.blomme@gmail.com>
2024-04-02 09:28:20 +0200

    This should do the trick??

--- JavaGateway.java
+++ JavaGateway.java
@@ -1,5 +1,8 @@
 import py4j.GatewayServer;
 import java.util.*;
+
+import javax.management.Query;
+
 import java.io.*;
 import org.w3c.dom.Element;
 import oasis.names.tc.saml._2_0.protocol.AttributeQuery;
@@ -16,6 +19,9 @@ import be.ehealth.business.mycarenetdomaincommons.domain.Attribute;
 import be.ehealth.businessconnector.genericasync.session.GenAsyncSessionServiceFactory;
 import be.ehealth.technicalconnector.utils.ConnectorIOUtils;
 import java.nio.charset.StandardCharsets;
+// import java.sql.Blob;
+import be.ehealth.business.mycarenetdomaincommons.domain.Blob;
+
 import static java.nio.charset.StandardCharsets.UTF_8;
 import be.ehealth.technicalconnector.exception.ConnectorException;
 import be.ehealth.business.mycarenetdomaincommons.domain.McnPackageInfo;
@@ -37,6 +43,32 @@ import org.slf4j.LoggerFactory;
 import be.ehealth.technicalconnector.idgenerator.IdGeneratorFactory;
 import org.slf4j.Logger;
 import be.cin.nip.async.generic.Confirm;
+import be.ehealth.businessconnector.mycarenet.agreement.builders.impl.EncryptedRequestObjectBuilderImpl;
+import be.ehealth.businessconnector.mycarenet.agreement.exception.AgreementBusinessConnectorException;
+import be.ehealth.business.mycarenetdomaincommons.domain.InputReference;
+import be.ehealth.business.common.domain.Patient;
+import org.joda.time.DateTime;
+import be.ehealth.businessconnector.mycarenet.agreement.domain.AskAgreementBuilderRequest;
+import be.ehealth.businessconnector.mycarenet.agreement.domain.AgreementBuilderRequest;
+import be.ehealth.business.mycarenetdomaincommons.builders.BlobBuilder;
+import be.ehealth.business.mycarenetdomaincommons.builders.BlobBuilderFactory;
+import be.ehealth.businessconnector.mycarenet.agreement.validator.AgreementXmlValidatorImpl;
+import be.ehealth.business.mycarenetcommons.v3.mapper.BlobMapper;
+import be.ehealth.businessconnector.mycarenet.agreement.exception.AgreementBusinessConnectorExceptionValues;
+import be.cin.encrypted.BusinessContent;
+import be.cin.encrypted.EncryptedKnownContent;
+import be.ehealth.technicalconnector.service.keydepot.KeyDepotManagerFactory;
+import be.ehealth.technicalconnector.service.keydepot.KeyDepotManager.EncryptionTokenType;
+import be.ehealth.technicalconnector.utils.ConnectorXmlUtils;
+import be.ehealth.technicalconnector.utils.SessionUtil;
+import be.ehealth.businessconnector.mycarenet.agreement.security.AgreementEncryptionUtil;
+import org.apache.commons.codec.binary.Base64;
+import be.fgov.ehealth.mycarenet.commons.protocol.v3.SendRequestType;
+import be.ehealth.technicalconnector.exception.TechnicalConnectorException;
+import be.ehealth.technicalconnector.exception.TechnicalConnectorExceptionValues;
+import be.ehealth.business.mycarenetcommons.v3.mapper.RoutingMapper;
+import be.fgov.ehealth.mycarenet.commons.core.v3.CommonInputType;
+import be.ehealth.business.mycarenetdomaincommons.domain.CommonInput;
 
 public class JavaGateway {
   private static final Logger LOG = LoggerFactory.getLogger(JavaGateway.class);
@@ -147,6 +179,68 @@ public class JavaGateway {
       confirm.setOrigin(((CommonInputMapper)Mappers.getMapper(CommonInputMapper.class)).map(commonBuilder.createOrigin(packageInfo)));
       confirm.getMsgRefValues().add(ref);
       return GenAsyncSessionServiceFactory.getGenAsyncService(projectName).confirmRequest(confirm, WsAddressingUtil.createHeader(null, "urn:be:cin:nip:async:generic:confirm:hash"));
+  }
+
+  public EncryptedRequestObjectBuilderImplWithRouting getEncryptedRequestObjectBuilderImplWithRouting() {
+    return new EncryptedRequestObjectBuilderImplWithRouting();
+  }
+  public class EncryptedRequestObjectBuilderImplWithRouting extends EncryptedRequestObjectBuilderImpl {
+    public AskAgreementBuilderRequest buildAskAgreementRequest(boolean isTest, boolean altRouting, InputReference reference, Patient patientInfo, DateTime refDate, byte[] messageFHIR) throws TechnicalConnectorException, AgreementBusinessConnectorException {
+        AskAgreementBuilderRequest requestBuilder = new AskAgreementBuilderRequest();
+        this.build(isTest, reference, patientInfo, refDate, messageFHIR, requestBuilder);
+        return requestBuilder;
+    }
+    private void checkParameterNotNull(Object references, String parameterName) throws AgreementBusinessConnectorException {
+      if (references == null) {
+         throw new AgreementBusinessConnectorException(AgreementBusinessConnectorExceptionValues.PARAMETER_NULL, new Object[]{parameterName});
+      }
+   }
+
+   public <T extends AgreementBuilderRequest> void build(boolean isTest, boolean altRouting, InputReference references, Patient patientInfo, DateTime refDate, byte[] messageFHIR, T requestBuilder) throws TechnicalConnectorException, AgreementBusinessConnectorException {
+      this.checkParameterNotNull(references, "InputReference");
+      this.checkParameterNotNull(references.getInputReference(), "Input reference");
+      String detailId = "_" + IdGeneratorFactory.getIdGenerator("uuid").generateId();
+      BlobBuilder blobBuilder = BlobBuilderFactory.getBlobBuilder("agreement");
+      BusinessContent businessContent = new BusinessContent();
+      businessContent.setId(detailId);
+      businessContent.setValue(messageFHIR);
+      requestBuilder.setBusinessContent(businessContent);
+      EncryptedKnownContent encryptedKnownContent = new EncryptedKnownContent();
+      encryptedKnownContent.setReplyToEtk(KeyDepotManagerFactory.getKeyDepotManager().getETK(EncryptionTokenType.HOLDER_OF_KEY).getEncoded());
+      encryptedKnownContent.setBusinessContent(businessContent);
+      encryptedKnownContent.setXades(buildXades(detailId, ConnectorXmlUtils.toByteArray(businessContent)));
+      if (LOG.isDebugEnabled()) {
+         ConnectorXmlUtils.dump(encryptedKnownContent);
+      }
+
+      Object var11 = null;
+
+      byte[] payload;
+      try {
+         payload = (new AgreementEncryptionUtil()).handleEncryption(encryptedKnownContent, SessionUtil.getHolderOfKeyCrypto());
+         if (payload != null && ConfigFactory.getConfigValidator().getBooleanProperty("be.ehealth.businessconnector.mycarenet.agreement.builders.impl.dumpMessages", false)) {
+            LOG.debug("EncryptedRequestObjectBuilder : Created blob content: " + Base64.encodeBase64String(payload));
+         }
+      } catch (Exception var15) {
+         throw new TechnicalConnectorException(TechnicalConnectorExceptionValues.ERROR_CRYPTO, var15, new Object[0]);
+      }
+
+      Blob blob = blobBuilder.build(payload, "none", detailId, "text/xml", (String)null, "encryptedForKnownBED");
+      SendRequestType request = requestBuilder.getRequest();
+      CommonBuilder commonBuilder = RequestBuilderFactory.getCommonBuilder("agreement");
+      
+      request.setCommonInput(((be.ehealth.business.mycarenetcommons.v3.mapper.CommonInputMapper)Mappers.getMapper(be.ehealth.business.mycarenetcommons.v3.mapper.CommonInputMapper.class)).map(commonBuilder.createCommonInput(McnConfigUtil.retrievePackageInfo("agreement"), isTest, references.getInputReference())));
+      if (altRouting){
+        request.setRouting(((RoutingMapper)Mappers.getMapper(RoutingMapper.class)).map(commonBuilder.createRoutingToMutuality(patientInfo.getMutuality(), refDate)));
+      } else {
+        request.setRouting(((RoutingMapper)Mappers.getMapper(RoutingMapper.class)).map(commonBuilder.createRouting(patientInfo, refDate)));
+      }
+      request.setId(IdGeneratorFactory.getIdGenerator("xsid").generateId());
+      request.setIssueInstant(new DateTime());
+      request.setDetail(BlobMapper.mapBlobTypefromBlob(blob));
+      (new AgreementXmlValidatorImpl()).validate(request);
+   }
+
   }
 
 }
